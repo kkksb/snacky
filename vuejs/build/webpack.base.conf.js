c3dfd1fa87e61345fac4e17a80468953473e8d3f
kkksb <kkex5027@gmail.com>
2022-04-08 22:34:07 +0900

    vuetifyを改めて導入

--- vuejs/build/webpack.base.conf.js
+++ vuejs/build/webpack.base.conf.js
@@ -74,6 +74,21 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require('sass'),
+              indentedSyntax: true // optional
+            }
+          }
+        ]
       }
     ]
   },
