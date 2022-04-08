d146d853dea3dfe7eadb6d209ab592696b544f18
kkksb <kkex5027@gmail.com>
2022-04-09 08:27:13 +0900

    add: メイン画面予定部分を作成

---
+++ vuejs/src/components/organisms/SideMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: 'Home' },
+        { title: 'About' },
+        { title: 'オススメのお店' }
+      ],
+      right: null
+    }
+  }
+}
+</script>
---
+++ vuejs/src/components/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-main>
+    <v-row>
+        <side-menu></side-menu>
+        <v-card width="1500px">
+          <l-map ref="map" :zoom="mapProps.zoom" :center="mapProps.center">
+            <l-tile-layer :url="mapProps.url"></l-tile-layer>
+          </l-map>
+        </v-card>
+    </v-row>
+ </v-main>
+</template>
+
+<script>
+import SideMenu from '@/components/organisms/SideMenu'
+import 'leaflet/dist/leaflet.css'
+import { LMap, LTileLayer } from 'vue2-leaflet'
+
+export default {
+  components: {
+    SideMenu,
+    LMap,
+    LTileLayer
+  },
+  data () {
+    return {
+      mapProps: {
+        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+        zoom: 8,
+        center: [34, 137]
+      }
+    }
+  }
+}
+</script>
