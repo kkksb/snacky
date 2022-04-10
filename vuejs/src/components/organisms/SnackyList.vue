ecc9588d633e1493e247f3de9e3f35ab46a6483e
kkksb <kkex5027@gmail.com>
2022-04-10 12:36:14 +0900

    add: リストを作成

---
+++ vuejs/src/components/organisms/SnackyList.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-list dense>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    selectedItem: 1,
+    items: [
+      { text: 'ここのお店おいしかった' },
+      { text: 'まずまずの味' },
+      { text: '店主がいい人だった' }
+    ]
+  })
+}
+</script>
--- vuejs/src/components/pages/Home.vue
+++ vuejs/src/components/pages/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <snack-map></snack-map>
+  <v-row>
+    <v-col cols="12" md="9">
+      <snack-map></snack-map>
+    </v-col>
+    <v-col cols="12" md="3">
+      <snacky-list></snacky-list>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import SnackMap from '@/components/organisms/Map'
+import SnackyList from '@/components/organisms/SnackyList'
 
 export default {
   components: {
-    SnackMap
+    SnackMap,
+    SnackyList
   }
 }
 </script>
